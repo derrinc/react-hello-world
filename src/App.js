9307abc3f34ba4ba10d3e450e0aefde89e065988
Charlie Sakamaki <csakamaki@revacomm.com>
2023-09-21 15:43:49 -1000

    Update hello world message

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>
+          <code> Hello World </code> from Alfred
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
